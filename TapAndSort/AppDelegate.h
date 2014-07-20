f9877edb177db0f8b3fa63b57a813f0c2796a7ee
Lam Nguyen <nhlam.225@gmail.com>
2014-07-20 16:01:03 +0700

    Initial Commit

---
+++ TapAndSort/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TapAndSort
+//
+//  Created by Gin on 7/20/14.
+//  Copyright (c) 2014 Nguyễn Huỳnh Lâm. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TapAndSort/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  TapAndSort
+//
+//  Created by Gin on 7/20/14.
+//  Copyright (c) 2014 Nguyễn Huỳnh Lâm. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
